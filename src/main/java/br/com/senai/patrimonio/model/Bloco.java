981c083bc5b93e6dce0385a696b2eeedd8957fa0
andrei_silva150 <andrei_silva150@estudante.sesisenai.org.br>
2026-08-17 21:57:08 -0300

    Cria classes Bloco e pessoa.

---
+++ src/main/java/br/com/senai/patrimonio/model/Bloco.java
@@ -0,0 +1,45 @@
+package br.com.senai.patrimonio.model;
+
+public class Bloco {
+    private Long id;
+    private String nome;
+    private Empresa empresa;
+
+    public Bloco() {
+    }
+
+    public Bloco(Long id, String nome, Empresa empresa) {
+        this.id = id;
+        this.nome = nome;
+        this.empresa = empresa;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
---
+++ src/main/java/br/com/senai/patrimonio/model/Pessoa.java
@@ -0,0 +1,40 @@
+package br.com.senai.patrimonio.model;
+
+public class Pessoa {
+    private long id;
+    private String nome;
+    private String cpf;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(long id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return cpf;
+    }
+
+    public void setCPF(String CPF) {
+        this.cpf = cpf;
+    }
+}
